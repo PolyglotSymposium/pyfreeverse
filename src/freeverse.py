dc6217bc171d575d8056d02023effbc78b7b802d
Kazark <kazark@zoho.com>
2014-10-22 20:30:35 -0400

    Rapidly prototyped some of the library
    + I am trying to get this library off the ground as fast as possible.
      However, in doing so, I am accruing technical debt.
    + The implementation code is messy, though not without a certain
      intentional structure.
    + The tests are fat. This is intentional at this stage, but it will get
      out of control extremely fast, so it needs to be watched carefully.

--- src/freeverse.py
+++ src/freeverse.py
@@ -1,24 +1,69 @@
+import inspect
+
+class Should:
+    def __init__(self, actual_value):
+        self.__actual_value = actual_value
+
+    def should_equal(self, expected_value):
+        if self.__actual_value != expected_value:
+            raise AssertionError('%s does not equal %s' % (self.__actual_value, expected_value))
+
 def should(description, function):
-    pass
+    return Phrase(description, lambda value: function(Should(value)), ())
 
 class Result:
-    def __init__(self, description, children=None):
+    def __init__(self, description, error, children=None):
         self.__description = description
+        self.__message = error
         self.__children = children
 
     def description(self):
         return self.__description
 
     def passed(self):
-        return True
+        return self.__message == ''
+
+    def message(self):
+        return self.__message
 
     def children(self):
         return self.__children
 
+class Phrase:
+    @classmethod
+    def make(self, obj):
+        if type(obj) == type(()):
+            return Phrase(*obj)
+        else:
+            return obj
+
+    def __init__(self, description, function, children):
+        self.__description = description
+        self.__function = function
+        self.__children = (Phrase.make(child) for child in children)
+
+    def run(self, parent_output=None):
+        passed = True
+        numargs = len(inspect.getargspec(self.__function)[0])
+        message = ''
+        try:
+            if numargs == 0:
+                output = self.__function()
+            else:
+                output = self.__function(parent_output)
+        except AssertionError as error:
+            passed = False
+            message = str(error)
+
+        return Result(self.__description, message, [child.run(output) for child in self.__children])
+
 class SpecsFor:
     def __init__(self, description):
-        pass
-    def add(self, description, function, *args):
-        pass
+        self.__description = description
+        self.__children = []
+
+    def add(self, description, function, *children):
+        self.__children.append((description, function, children))
+
     def run(self):
-        return Result('True', [Result('be true')])
+        return Phrase(self.__description, lambda: None, self.__children).run()
--- src/freeverse_tests.py
+++ src/freeverse_tests.py
@@ -4,18 +4,34 @@ import unittest
 
 from freeverse import SpecsFor, should
 
-specs = SpecsFor('Tests for the Freeverse spec library')
-
 class FreeverseTests(unittest.TestCase):
     def test_simplest_passing_case(self):
-        specs.add('True', lambda: True, should('be true', lambda t: t.should_be(True)))
+        specs = SpecsFor('Tests for the Freeverse spec library')
+        specs.add('True', lambda: True, should('be true', lambda t: t.should_equal(True)))
 
         result = specs.run()
 
+        self.assertEqual('Tests for the Freeverse spec library', result.description())
+        result = result.children()[0]
         self.assertEqual('True', result.description())
         self.assertTrue(result.passed())
-        self.assertEqual('be true', result.children()[0].description())
-        self.assertTrue(result.children()[0].passed())
+        first_child = result.children()[0]
+        self.assertEqual('be true', first_child.description())
+        self.assertTrue(first_child.passed())
+        self.assertEqual('', first_child.message())
+
+    def test_simplest_failing_case(self):
+        specs = SpecsFor('Tests for the Freeverse spec library')
+        specs.add('Two', lambda: 2, should('be false', lambda t: t.should_equal(False)))
+
+        result = specs.run().children()[0]
+
+        self.assertEqual('Two', result.description())
+        self.assertTrue(result.passed())
+        first_child = result.children()[0]
+        self.assertEqual('be false', first_child.description())
+        self.assertFalse(first_child.passed())
+        self.assertEqual('2 does not equal False', first_child.message())
 
 if __name__ == '__main__':
     unittest.main()
