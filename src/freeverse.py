51f4f9ca1395aa4f777a093e0a3e9fdeec5ff444
Kazark <kazark@zoho.com>
2014-10-24 19:21:47 -0400

    Added some tests and fixed a bug in "It"
    Bit by Python closures, again! Seems I get pit by them every time I try
    to use them in a loop. Reminiscent of C# problems...

--- src/freeverse.py
+++ src/freeverse.py
@@ -2,6 +2,12 @@ class ActualValue:
     def __init__(self, actual_value):
         self.__actual_value = actual_value
 
+    def should(self, predicate):
+        if predicate(self.__actual_value):
+            return ''
+        else:
+            return 'Predicate not true of %s' % self.__actual_value
+
     def should_equal(self, expected_value):
         if self.__actual_value == expected_value:
             return ''
@@ -21,9 +27,11 @@ class Expect:
 It = lambda: It
 for method_name in dir(ActualValue):
     if method_name.startswith('should'):
-        setattr(It, method_name,
-            lambda expected:
-                lambda actual_value: getattr(actual_value, method_name)(expected))
+        (lambda method_name:
+            setattr(It, method_name,
+                lambda expected:
+                    lambda actual_value: getattr(actual_value, method_name)(expected))
+        )(method_name)
 
 class Result:
     def __init__(self, description, error, children=None):
--- src/freeverse_tests.py
+++ src/freeverse_tests.py
@@ -2,7 +2,22 @@
 
 import unittest
 
-from freeverse import SpecsFor, Should, Expect, It
+from freeverse import ActualValue, SpecsFor, Should, Expect, It
+
+class ShouldStyleAssertionsTests(unittest.TestCase):
+    def test_basic_Actual_Value_should_method_takes_predicate(self):
+        self.assertEqual('', ActualValue(4).should(lambda actual: actual == 4))
+        self.assertEqual('Predicate not true of 4', ActualValue(4).should(lambda actual: actual == 2))
+
+class ExpectStyleAssertionsTests(unittest.TestCase):
+    def test_basic_Expect_to_method_takes_predicate(self):
+        self.assertEqual('', Expect(4).to(lambda actual: actual == 4))
+        self.assertGreater(len(Expect(4).to(lambda actual: actual == 2)), 0)
+
+class ItStyleAssertionsTests(unittest.TestCase):
+    def test_basic_It_should_method_takes_predicate(self):
+        self.assertEqual('', It.should(lambda actual: actual == 4)(ActualValue(4)))
+        self.assertGreater(len(It.should(lambda actual: actual == 2)(ActualValue(4))), 0)
 
 class FreeverseTests(unittest.TestCase):
     def test_simplest_passing_case(self):
