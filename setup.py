6e6c8aa3fa3e0d8dc575916983dd88996ec80d77
Kazark <kazark@zoho.com>
2014-10-25 14:39:23 -0400

    Step #2 of making Freeverse a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='freeverse',
+    version='0.0.0',
+    description='A new way to write free-form, minimalistic specs (tests) in Python that favor natural language',
+    url='https://github.com/Kazark/pyfreeverse',
+    author='Kazark',
+    author_email='kazark@zoho.com',
+    license='MIT',
+    packages=[],
+    zip_safe=False)
